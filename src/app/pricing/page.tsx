778006e546148cffce8ab51dfa259fea829cc43a
Kevin Downey <downey.kevin@gmail.com>
2026-04-02 13:25:59 -0400

    Update pricing font size

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -105,7 +105,7 @@ export default function Page() {
                 <div className="rounded-2xl bg-gray-50 py-10 text-center inset-ring inset-ring-gray-900/5 dark:bg-gray-900 dark:inset-ring-white/10 lg:flex lg:flex-col lg:justify-center lg:py-16">
                   <div className="mx-auto max-w-xs px-8">
                     <p className="text-base font-semibold text-slate-600 dark:text-slate-400">Flat monthly license</p>
-                    <p className="mt-6 text-5xl font-normal tracking-tight text-primary dark:text-slate-100">Contact Us</p>
+                    <p className="mt-6 text-4xl font-normal tracking-tight text-primary dark:text-slate-100">Contact Us</p>
                     <a
                       href="/request-access"
                       className="mt-10 block w-full rounded-md bg-primary px-3 py-2 text-center text-sm font-semibold text-white shadow-xs hover:bg-accent focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary/30 dark:bg-[#3A4F7A] dark:text-white dark:hover:bg-[#4D6391] dark:focus-visible:outline-[#3A4F7A]"

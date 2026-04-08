0b9cae7cc88a321556f3da2b443949caf30b87e4
Kevin Downey <downey.kevin@gmail.com>
2026-04-08 00:08:32 -0400

    Adjust formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
             <AnnouncementBadge
               href="https://www.protos.fi/blog/introducing-arche-deterministic-edgar-intelligence-for-developers"
               text="Introducing Arche"
-              className="[&>span:first-child]:font-normal"
+              className="[&>span:first-child]:font-normal [&>span:last-child]:font-normal"
             />
           }
           headline="Financial fundamentals you can defend."
